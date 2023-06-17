fc5f01c72ab3fc44fa1e338998bc0fd8ad53e66e
Caio-Kaefer <kaefer.caio@gmail.com>
2023-06-17 01:20:19 -0300

    correcao de problemas com o git, estilizacao do projeto e adicao de um formulario para inserir dados com a api

---
+++ src/PostForm.jsx
@@ -0,0 +1,198 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const FormularioPostAPI = () => {
+  const [dadosFormulario, setDadosFormulario] = useState({
+    processo: 0,
+    processoAno: 0,
+    modalidade: '',
+    tipo: '',
+    localDesembaraco: '',
+    canal: '',
+    due: '',
+    dataRegistroDue: '',
+    exportador: '',
+    fatura: '',
+    dataDeAbertura: '',
+    dataDeEmbarque: '',
+    descricao: ''
+  });
+
+  const handleChange = (event) => {
+    setDadosFormulario({
+      ...dadosFormulario,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+
+    try {
+      const response = await axios.post('https://portal.comex360.idata.com.br:8789/process', dadosFormulario);
+      console.log(response.data); // Tratar a resposta da API como desejado
+      console.log(response.status);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={styles.form}>
+      <h2>Inserir Dados</h2>
+      <div style={styles.formGroup}>
+        <label>Processo:</label>
+        <input
+          type="number"
+          name="processo"
+          value={dadosFormulario.processo}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Ano do Processo:</label>
+        <input
+          type="number"
+          name="processoAno"
+          value={dadosFormulario.processoAno}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Modalidade:</label>
+        <input
+          type="text"
+          name="modalidade"
+          value={dadosFormulario.modalidade}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Tipo:</label>
+        <input
+          type="text"
+          name="tipo"
+          value={dadosFormulario.tipo}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Local de Desembaraço:</label>
+        <input
+          type="text"
+          name="localDesembaraco"
+          value={dadosFormulario.localDesembaraco}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Canal:</label>
+        <input
+          type="text"
+          name="canal"
+          value={dadosFormulario.canal}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>DUE:</label>
+        <input
+          type="text"
+          name="due"
+          value={dadosFormulario.due}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Data de Registro da DUE:</label>
+        <input
+          type="datetime-local"
+          name="dataRegistroDue"
+          value={dadosFormulario.dataRegistroDue}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Exportador:</label>
+        <input
+          type="text"
+          name="exportador"
+          value={dadosFormulario.exportador}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Fatura:</label>
+        <input
+          type="text"
+          name="fatura"
+          value={dadosFormulario.fatura}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Data de Abertura:</label>
+        <input
+          type="datetime-local"
+          name="dataDeAbertura"
+          value={dadosFormulario.dataDeAbertura}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Data de Embarque:</label>
+        <input
+          type="datetime-local"
+          name="dataDeEmbarque"
+          value={dadosFormulario.dataDeEmbarque}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <label>Descrição:</label>
+        <textarea
+          name="descricao"
+          value={dadosFormulario.descricao}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <button type="submit" style={styles.submitButton}>
+          Enviar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const styles = {
+  form: {
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    background: '#f9f9f9'
+  },
+  formGroup: {
+    marginBottom: '15px',
+    textAlign: 'right' // Alinha os inputs à esquerda
+  },
+  submitButton: {
+    padding: '10px 20px',
+    background: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    fontWeight: 'bold'
+  }
+};
+
+
+
+
+
+export default FormularioPostAPI;
